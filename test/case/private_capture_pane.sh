60c1a967ff3e4442028cf90f32e7ce8f67bc562f
Jori Bomanson <jori.bomanson@gmail.com>
2020-08-19 12:24:09 +0300

    Fixup to Add a unit test framework

--- test/case/private_capture_pane.sh
+++ test/case/private_capture_pane.sh
@@ -2,7 +2,7 @@
 
 source "${0%/*}/../lib/prelude.sh"
 
-tmux send-keys "echo Hello world" Enter
+tmux send-keys -t "=$test_session:0.0" "echo Hello world" Enter
 
 tmux_kak_copy_mode private_capture_pane -t "=$test_session:0.0" >pane.txt
 
