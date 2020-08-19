218cab675aa632e41c9a56fc58ba9a21ca12f631
Jori Bomanson <jori.bomanson@gmail.com>
2020-08-19 16:48:43 +0300

    Use sleep instead of tmux wait-for in tests

--- test/case/tmux_kak_config.sh
+++ test/case/tmux_kak_config.sh
@@ -5,9 +5,8 @@ source "${0%/*}/../lib/prelude.sh"
 touch config.txt
 tmux send-keys -t "=$test_session:0.0" \
 "kak -n -f '|printf \"%s\" \"\$kak_config\"<ret>' config.txt >/dev/null
-tmux wait-for -S filter
 "
-tmux wait-for filter
+sleep 1
 test_in_place config.txt sed "s,$PWD,€PWD,g"
 
 >config.txt.expected echo "€PWD/.config/kak"
