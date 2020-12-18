4cb0ecbeb17ce05a7fcb1534fbfdbe03da42a416
Jori Bomanson <jori.bomanson@gmail.com>
2020-12-18 18:28:46 +0200

    Make prelude.sh pass env vars to a new tmux session using the env program

--- test/lib/prelude.sh
+++ test/lib/prelude.sh
@@ -4,44 +4,52 @@ repository_root="${repository_root%/test*}"
 test_name="${0##*/}"
 test_root="$PWD"
 
+# A space delimited sequence of assignments to be used as arguments to the
+# env command.
+variable_assignments=""
+
+prelude_export ()
+{
+    variable_assignments="$variable_assignments $1=\"\$$1\""
+    export "$1"
+}
+
 # Export the test_root directory with a safe name.
 TMUX_KAK_COPY_MODE_TEST_ROOT="$test_root"
-export TMUX_KAK_COPY_MODE_TEST_ROOT
+prelude_export TMUX_KAK_COPY_MODE_TEST_ROOT
 
-# Export the current PATH before modifying it.
+# Export a copy of the current PATH before modifying it.
 TMUX_KAK_COPY_MODE_TEST_ORIGINAL_PATH="$PATH"
-export TMUX_KAK_COPY_MODE_TEST_ORIGINAL_PATH
+prelude_export TMUX_KAK_COPY_MODE_TEST_ORIGINAL_PATH
 
 # Set up a home directory specific to this test.
 HOME="$test_root/home"
-export HOME
+prelude_export HOME
 mkdir "$HOME"
 
 # Modify PATH to include the binary directory of this repository.
 PATH="$repository_root/bin:$PATH"
-export PATH
-
 # Modify PATH to include a directory for test helper binaries.
 PATH="$repository_root/test/bin:$PATH"
-export PATH
+prelude_export PATH
 
 # The shell used by tmux for new windows.
 SHELL="/bin/sh"
-export SHELL
+prelude_export SHELL
 
 # Set the path to kakoune user session sockets.
 XDG_RUNTIME_DIR="$test_root/xdg_runtime_dir"
-export XDG_RUNTIME_DIR
+prelude_export XDG_RUNTIME_DIR
 mkdir "$XDG_RUNTIME_DIR"
 
 # See KAKOUNE_CONFIG_DIR.
 XDG_CONFIG_HOME="$test_root/.config"
-export XDG_CONFIG_HOME
+prelude_export XDG_CONFIG_HOME
 mkdir "$XDG_CONFIG_HOME"
 
 # Set the path to kakoune user configuration.
 KAKOUNE_CONFIG_DIR="$XDG_CONFIG_HOME/kak"
-export KAKOUNE_CONFIG_DIR
+prelude_export KAKOUNE_CONFIG_DIR
 mkdir "$KAKOUNE_CONFIG_DIR"
 
 # Example:
@@ -72,4 +80,4 @@ trap test_clean_up EXIT
 
 # Start a new tmux session.
 test_session="TMUX_KAK_COPY_MODE_TEST_SESSION"
-tmux new-session -d -s "$test_session" -x80 -y10 /bin/sh
+eval tmux new-session -d -s '"$test_session"' -x80 -y10 -- env $variable_assignments /bin/sh
