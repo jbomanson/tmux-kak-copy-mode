46cdb1abcbfaa76353021e4a58a97f7c45792335
Jori Bomanson <jori.bomanson@gmail.com>
2021-01-03 11:05:43 +0200

    Test in_new_window

---
+++ test/case/in_new_window.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+source "${0%/*}/../lib/prelude.sh"
+
+tmux send-keys -t "=$test_session:0.0" 'echo Hello world' Enter
+tmux send-keys -t "=$test_session:0.0" 'tmux_kak_copy_mode in_new_window' Enter
+test_sleep_for_tmux
+tmux send-keys -t "=$test_session:0.0" ':execute-keys %(%sworld<ret>cthere<esc>)' Enter
+tmux send-keys -t "=$test_session:0.0" 'gk' Enter
+
+test_sleep_for_tmux
+
+# Capture and test the contents of the kakoune buffer.
+tmux capture-pane -t "=$test_session:0.0" -p -E8 >pane.txt
+>pane.txt.expected echo \
+"$ echo Hello there
+Hello there
+$ tmux_kak_copy_mode in_new_window
+
+
+
+
+
+"
+
+tmux list-windows -a >windows.info
+wc --l windows.info >windows.count
+>windows.count.expected echo "2 windows.info"
