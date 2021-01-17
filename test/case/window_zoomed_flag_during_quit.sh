27ad23770983ea471349a9dd72527d5d42d43fd3
Jori Bomanson <jori.bomanson@gmail.com>
2021-01-17 11:50:39 +0200

    Test window_zoom_flag more thoroughly

---
+++ test/case/window_zoomed_flag_during_quit.sh
@@ -0,0 +1,40 @@
+#! /bin/bash
+
+source "${0%/*}/../lib/prelude.sh"
+
+# Create another pane just so that this is not the only one.
+tmux split-pane -d -h
+
+tmux send-keys -t "=$test_session:0.0" 'echo Hello world' Enter
+tmux send-keys -t "=$test_session:0.0" 'tmux_kak_copy_mode in_new_window' Enter
+test_sleep_for_tmux
+
+# Zoom the copy mode pane.
+tmux resize-pane -Z
+
+tmux send-keys -t "=$test_session:0.0" ':execute-keys %(%sworld<ret>cthere<esc>)' Enter
+tmux send-keys -t "=$test_session:0.0" 'gk' Enter
+
+tmux send-keys -t "=$test_session:0.0" ':wq' Enter
+
+test_sleep_for_tmux
+
+# Capture and test the contents of the kakoune buffer.
+tmux capture-pane -t "=$test_session:0.0" -p -E8 >pane.txt
+>pane.txt.expected echo \
+"$ echo Hello world
+Hello world
+$ tmux_kak_copy_mode in_new_window
+$
+
+
+
+
+"
+
+tmux display-message -p "#{window_zoomed_flag}" >copy_mode.window_zoomed_flag.txt
+>copy_mode.window_zoomed_flag.txt.expected echo "1"
+
+tmux list-windows -a >windows.info
+wc --l windows.info >windows.count
+>windows.count.expected echo "1 windows.info"
